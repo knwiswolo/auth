60a9ec0ebbf542894aab3d9578a44365e7eb034b
maverick_369 <maverick_369@github.com>
2018-02-27 14:41:37 +0100

    final commit, switch-case and signout

--- src/App.js
+++ src/App.js
@@ -1,31 +1,69 @@
 // import libraries
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import firebase from 'firebase';
-import { Header } from './components/common';
+import firebase from 'firebase'; // import local firebase library
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 // create component: class-based
 class App extends Component {
+    state = { loggedIn: null };
+
     componentWillMount() {
-        firebase.initializeApp({
+        firebase.initializeApp({ // initialise firebase
             apiKey: 'AIzaSyD0MZtaEKjNxispt6SlsQYhApm_R3uzlrY',
             authDomain: 'auth-72893.firebaseapp.com',
             databaseURL: 'https://auth-72893.firebaseio.com',
             projectId: 'auth-72893',
             storageBucket: 'auth-72893.appspot.com',
             messagingSenderId: '678622546648'
-          });
+        });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
     }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+        return (
+            <Button onPress={() => firebase.auth().signOut()}>
+                Log Out
+            </Button>
+        );
+            case false:
+                return <LoginForm />;
+            default: 
+        return (
+            <View style={styles.viewStyle}>
+                <Spinner size="large" />
+            </View>        
+        );
+        }
+    }
+
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     }
 }
+
+const styles = {
+    viewStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row'
+    }
+};
 
 // make available to other components
 export default App;
